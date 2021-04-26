7acfa37166451f30dbc00f1be33388661fbd06be
Mark Williams <w_mark81@yahoo.com>
2021-04-25 20:13:13 -0500

    added Order of tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { ChakraProvider, chakra } from "@chakra-ui/react";
+import { Button, ButtonGroup, Box } from "@chakra-ui/react";
+
+const Card = chakra("button", {
+  baseStyle: {
+    shadow: "lg",
+    rounded: "lg",
+    bg: "white",
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <ChakraProvider>
+        <h1>Cheese</h1>
+        <Card>Butt</Card>
+        <Button opacity="0.5" bg="tomato">
+          Button
+        </Button>
+        <chakra.button
+          px="3"
+          py="2"
+          bg="green.200"
+          rounded="md"
+          _hover={{ bg: "green.300" }}
         >
-          Learn React
-        </a>
-      </header>
+          Click me
+        </chakra.button>
+        <Box
+          w="100%"
+          h="200px"
+          bgGradient="radial(gray.300,yellow.400,pink.200)"
+        />
+      </ChakraProvider>
     </div>
   );
 }
